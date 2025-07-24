daf4def3adf35d2ab887a7b4a2452e2ae1fd4af6
xyeah0 <3048300445@qq.com>
2025-07-25 07:33:21 +0800

    vite 路径

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/JPreader/', // 例如 '/reader/'
   plugins: [
     vue(),
     // vueDevTools(),
